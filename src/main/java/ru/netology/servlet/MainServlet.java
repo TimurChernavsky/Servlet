44c49f6bc81b2292101fcef241e371993651ebf3
Тимур <120785772+TimurChernavsky@users.noreply.github.com>
2023-06-27 09:48:47 +0300

    Update MainServlet.java

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,13 +1,3 @@
-package ru.netology.servlet;
-
-import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 public class MainServlet extends HttpServlet {
   private PostController controller;
 
@@ -28,18 +18,15 @@ public class MainServlet extends HttpServlet {
       if (method.equals("GET") && path.equals("/api/posts")) {
         controller.all(resp);
         return;
-      }
-      if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
+      } else if (method.equals("GET") && path.matches("/api/posts/\\d+")) {
         // easy way
         final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
         controller.getById(id, resp);
         return;
-      }
-      if (method.equals("POST") && path.equals("/api/posts")) {
+      } else if (method.equals("POST") && path.equals("/api/posts")) {
         controller.save(req.getReader(), resp);
         return;
-      }
-      if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
+      } else if (method.equals("DELETE") && path.matches("/api/posts/\\d+")) {
         // easy way
         final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
         controller.removeById(id, resp);
