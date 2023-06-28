06adf7e2a029dd3ef7788976d3e393a2c48dabcc
Тимур <120785772+TimurChernavsky@users.noreply.github.com>
2023-06-28 18:23:39 +0300

    Update MainServlet.java

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -13,9 +13,8 @@ public class MainServlet extends HttpServlet {
 
   @Override
   public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
+  final var context = new AnnotationConfigApplicationContext("ru.netology");
+        postController = context.getBean(PostController.class);
   }
 
   @Override
