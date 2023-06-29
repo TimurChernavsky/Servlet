9b27443dd33ce89b10930c6c7282746ac686719c
Тимур <120785772+TimurChernavsky@users.noreply.github.com>
2023-06-29 09:35:26 +0300

    Update PostRepository.java

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -5,10 +5,10 @@ import ru.netology.model.Post;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicLong;
 
-// Stub
+
 public class PostRepository {
-    private Map<Long, Post> list;
-    private AtomicLong postID;
+    private final Map<Long, Post> list = new HashMap<>();
+    private final AtomicLong postID = new AtomicLong();
 
     public List<Post> all() {
         return new ArrayList<>(list.values());
@@ -20,13 +20,17 @@ public class PostRepository {
     }
 
     public Post save(Post post) {
-        Random random = new Random();
-        postID.addAndGet(random.nextLong());
+        postID.incrementAndGet();
         list.put(postID.get(), post);
         return post;
     }
 
     public void removeById(long id) {
-        list.remove(id);
+        try {
+            list.remove(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
+
