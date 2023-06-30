43c10cd278c7be5c3f26b974a09c3bfccb812926
Тимур <120785772+TimurChernavsky@users.noreply.github.com>
2023-06-30 09:44:22 +0300

    Update MainServlet.java

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -9,49 +9,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class MainServlet extends HttpServlet {
-    private PostController controller;
-    private String GET = "GET";
-    private String api = "/api/posts";
-    private String POST = "POST";
-    private String DELETE = "DELETE";
+    private static PostController controller;
+    private static String METHOD_GET = "GET";
+    private static String METHOD_DELETE = "DELETE";
+    private static String METHOD_POST = "POST";
 
+    private static String WAY_api_posts = "/api/posts";
 
-  @Override
-  public void init() {
-    final var repository = new PostRepository();
-    final var service = new PostService(repository);
-    controller = new PostController(service);
-  }
-
-  @Override
-  protected void service(HttpServletRequest req, HttpServletResponse resp) {
-    // если деплоились в root context, то достаточно этого
-    try {
-      final var path = req.getRequestURI();
-      final var method = req.getMethod();
-      // primitive routing
-      if (method.equals(GET) && path.equals(api)) {
-        controller.all(resp);
-        return;
-      } else if (method.equals(GET) && path.matches(api + "\\d")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.getById(id, resp);
-        return;
-      } else if (method.equals(POST) && path.equals(api)) {
-        controller.save(req.getReader(), resp);
-        return;
-      } else if (method.equals(DELETE) && path.matches(api + "\\d")) {
-        // easy way
-        final var id = Long.parseLong(path.substring(path.lastIndexOf("/")));
-        controller.removeById(id, resp);
-        return;
-      }
-      resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    } catch (Exception e) {
-      e.printStackTrace();
-      resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    @Controller
+    public void init() {
+        final var context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        controller = context.getBean(PostController.class);
     }
-  }
-}
 
+    private Long getID(String path) {
+        return Long.parseLong(path.substring(path.lastIndexOf("/")));
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) {
+        // если деплоились в root context, то достаточно этого
+        try {
+            final var path = req.getRequestURI();
+            final var method = req.getMethod();
+            // primitive routing
+            if (method.equals(METHOD_GET) && path.equals(WAY_api_posts)) {
+                controller.all(resp);
+                return;
+            } else if (method.equals(METHOD_GET) && path.matches(WAY_api_posts + "\\d")) {
+                // easy way
+                final var id = getID(path);
+                controller.getById(id, resp);
+                return;
+            } else if (method.equals(METHOD_POST) && path.equals(WAY_api_posts)) {
+                controller.save(req.getReader(), resp);
+                return;
+            } else if (method.equals(METHOD_DELETE) && path.matches(WAY_api_posts + "\\d")) {
+                // easy way
+                final var id = getID(path);
+                controller.removeById(id, resp);
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
