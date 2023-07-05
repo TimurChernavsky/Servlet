b2e97395f8c37e163179f0e3a79b9cdf0441a421
Тимур <120785772+TimurChernavsky@users.noreply.github.com>
2023-07-05 17:13:49 +0300

    Update PostRepository.java

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -2,34 +2,16 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
+import java.util.Optional;
 
-public class PostRepository {
-    private final ConcrurrentMap<Long, Post> list = new ConcrurrentHashMap<>();
-    private final AtomicLong postID = new AtomicLong();
+public interface PostRepository {
+  List<Post> all();
 
-    public List<Post> all() {
-        return new ArrayList<>(list.values());
+  Optional<Post> getById(long id);
 
-    }
+  Post save(Post post);
 
-    public Optional<Post> getById(long id) {
-        return Optional.ofNullable(list.get(id));
-    }
-
-    public Post save(Post post) {
-        postID.incrementAndGet();
-        list.put(postID.get(), post);
-        return post;
-    }
-
-    public void removeById(long id) {
-        try {
-            list.remove(id);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+  void removeById(long id);
 }
 
